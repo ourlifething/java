3c6429544118abce4431333947ce9e8ec55d9062
ourlifething <ryuji.hayama@gmail.com>
2023-05-29 10:56:57 +0900

    自己紹介アプリ ver1

--- IntroductionApp.java
+++ IntroductionApp.java
@@ -1,9 +1,9 @@
 import java.util.*;
 public class IntroductionApp{
 	public static void main(String[] args){
-		String[] params={"体力","魔力","パワー","きようさ","すばやさ"};
-		String[] races={"人間","ハイエルフ","トロル","ノーム"};
-		String[] classes={"戦士","盗賊","僧侶","魔術師"};
+		String[] params = {"体力","魔力","パワー","きようさ","すばやさ"};
+		String[] races = {"人間","ハイエルフ","トロル","ノーム"};
+		String[] classes = {"戦士","盗賊","僧侶","魔術師"};
 		int[][] raceMatrix={
 			{10,10,10,10,10},
 			{0,20,0,10,20},
@@ -23,6 +23,26 @@ public class IntroductionApp{
 		int[] status = makeStatus(seed, new int[] {100,100,50,50,50});
 		System.out.println("初期ステータスが決定しました");
 		showStatus(status,params);
+		System.out.printf("種族を選んでください >> (%s) >>",arrToString(races)); //arrTostring()作成
+		int race = sc.nextInt();
+		System.out.println(races[race] + "のボーナスが適用されました");
+		raceBonus(status, raceMatrix[race]);
+		showStatus(status,params);
+		
+		System.out.printf("職業を選んでください >> (%s) >>",arrToString(classes)); //arrTostring()作成
+		int cls = sc.nextInt();
+		System.out.println(classes[cls] + "のボーナスが適用されました");
+		classBonus(status, classMatrix[cls]);
+		showStatus(status,params);
+
+		System.out.println("***作成成功！***");
+		System.out.printf("私は%sの%s、%sです。%n",classes[cls],races[race],name);
+		System.out.println("能力値(" + sumStatus(status) + ")");
+		//sortStatus(status, params);
+		for(int i =0; i < status.length; i++){
+			System.out.printf("%s:%d%n",params[i],status[i]);
+		}
+		System.out.println("です。今後ともよろしく。。。");
 	}
 	static int calcseed(String name){
 		//seed(種)
@@ -65,5 +85,14 @@ public class IntroductionApp{
 			str += params[i] + ":" + status[i] + " ";
 		}
 		System.out.println( "[" + str + "]" );
+	}
+	static String arrToString(String[] arr){
+		String str = "";
+		for(int i =0; i < arr.length; i++){
+			//str += i + "..." + arr[i]+","; 
+			str += String.format("%d...%s,",i,arr[i]); //String.format書式を使った文字列を返す
+		}
+		str = str.substring(0, str.length()-1); //最後の","削除
+		return str;
 	}
 }
